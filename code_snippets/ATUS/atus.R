550624fd317886a25bfe8d72b57e4893454fad47
Nick Tallant <ndtallant@uchicago.edu>
2018-08-29 20:10:49 -0500

    Cleaning up

--- code_snippets/ATUS/atus.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-data <- read.csv('atuscps_2017.dat')
-data$PEIO1ICD <- as.character(data$PEIO1ICD)
-by_region <- data %>%
-  group_by(GEDIV, PEIO1ICD) %>%
-  summarize(industry.count = count(PEIO1ICD)) 
-
--- code_snippets/ATUS/atus.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('atuscps_2017.dat')
-by_state = df.GESTFIPS.value_counts(normalize=True).sort_index()
-
-print('Share of observations by state (FIPS)')
-print(by_state)
